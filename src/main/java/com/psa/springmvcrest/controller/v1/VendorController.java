4e4365562e2fa86bb2b3daada7796f108aeed095
pyaesoneaung <pyaesoneaung.ucsy@gmail.com>
2021-08-04 15:08:34 +0630

    feat: Swagger with Spring MVC

--- src/main/java/com/psa/springmvcrest/controller/v1/VendorController.java
+++ src/main/java/com/psa/springmvcrest/controller/v1/VendorController.java
@@ -3,12 +3,15 @@ package com.psa.springmvcrest.controller.v1;
 import com.psa.springmvcrest.api.v1.model.VendorDTO;
 import com.psa.springmvcrest.api.v1.model.VendorListDTO;
 import com.psa.springmvcrest.services.VendorService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
 /**
  * Created by PSA at 7/3/2021
  */
+@Api(description = "This is vendor API")
 @RestController
 @RequestMapping(VendorController.BASE_URL)
 public class VendorController {
@@ -20,36 +23,42 @@ public class VendorController {
         this.vendorService = vendorService;
     }
 
+    @ApiOperation(value = "View list of Vendor",notes = "These are some API notes")
     @GetMapping
     @ResponseStatus(HttpStatus.OK)
     public VendorListDTO getallVendors(){
         return vendorService.getAllVendors();
     }
 
+    @ApiOperation(value = "Get Vendor by Id")
     @GetMapping({"/{id}"})
     @ResponseStatus(HttpStatus.OK)
     public VendorDTO getVendorById(@PathVariable Long id){
         return vendorService.getVendorById(id);
     }
 
+    @ApiOperation(value = "Create a new Vendor")
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
     public VendorDTO createNewVendor(@RequestBody VendorDTO vendorDTO){
         return vendorService.createNewVendor(vendorDTO);
     }
 
+    @ApiOperation(value = "Update an existing Vendor")
     @PutMapping({"/{id}"})
     @ResponseStatus(HttpStatus.OK)
     public VendorDTO updateVendor(@PathVariable Long id, @RequestBody VendorDTO vendorDTO){
         return vendorService.saveVendorByDTO(id, vendorDTO);
     }
 
+    @ApiOperation(value = "Update a Vendor Property")
     @PatchMapping({"/{id}"})
     @ResponseStatus(HttpStatus.OK)
     public VendorDTO patchVendor(@PathVariable Long id, @RequestBody VendorDTO vendorDTO){
         return vendorService.patchVendor(id, vendorDTO);
     }
 
+    @ApiOperation(value = "Delete a Vendor")
     @DeleteMapping({"/{id}"})
     @ResponseStatus(HttpStatus.OK)
     public void deleteVendor(@PathVariable Long id){
